8b7c59e4db3b60238be8ce6df229261afd0a90d6
p.mironychev <artifeks92@gmail.com>
2020-02-09 12:51:06 +0300

    Update install.bat

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -9,3 +9,12 @@ echo MSYS2 directory: %MSYS2_DIR%
 echo MSYS2 system: %MSYSTEM%
 echo Configuration: %CONFIGURATION%
 echo Bits: %BIT%
+
+IF %COMPILER%==msys2 (
+  @echo on
+  SET "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
+  bash -lc "pacman --noconfirm --sync pacman-mirrors"
+  bash -lc "pacman -S --needed --noconfirm git"
+  bash -lc "pacman -S --needed --noconfirm mingw-w64-x86_64-gcc"
+  bash -lc "pacman -S --needed --noconfirm mingw-w64-x86_64-toolchain mingw-w64-x86_64-make"
+)
