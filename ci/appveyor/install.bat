43870eca25ce07fd6f58a3030168a73a7fcce48c
p.mironychev <artifeks92@gmail.com>
2020-02-09 12:41:21 +0300

    Update appveyor.yml, build.bat, and install.bat

---
+++ ci/appveyor/install.bat
@@ -0,0 +1,11 @@
+@echo off
+
+cd %APPVEYOR_BUILD_FOLDER%
+
+echo Compiler: %COMPILER%
+echo Architecture: %MSYS2_ARCH%
+echo Platform: %PLATFORM%
+echo MSYS2 directory: %MSYS2_DIR%
+echo MSYS2 system: %MSYSTEM%
+echo Configuration: %CONFIGURATION%
+echo Bits: %BIT%
